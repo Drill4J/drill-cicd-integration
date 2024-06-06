a70c75173a52cb59907762f0ee1f027aeb585513
Bohdan <bohdan_asieiev@epam.com>
2024-06-06 16:42:01 +0300

    feat: resolve signing for publishing to maven-central

--- drill-cicd-maven-plugin/build.gradle.kts
+++ drill-cicd-maven-plugin/build.gradle.kts
@@ -1,12 +1,12 @@
 import com.hierynomus.gradle.license.tasks.LicenseCheck
 import com.hierynomus.gradle.license.tasks.LicenseFormat
 import org.jetbrains.kotlin.konan.target.HostManager
-import org.jetbrains.kotlin.konan.target.presetName
-import java.lang.System
 import java.net.URI
 
 plugins {
+    signing
     `maven-publish`
+    kotlin("jvm")
     id("com.github.hierynomus.license")
 }
 
@@ -14,32 +14,67 @@ group = "com.epam.drill.integration"
 version = rootProject.version
 
 val kotlinVersion: String by extra
+val ktorVersion: String by extra
 
 repositories {
     mavenLocal()
     mavenCentral()
 }
 
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlinx.serialization.ExperimentalSerializationApi")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.apache.maven:maven-core:3.8.1")
+    implementation("org.apache.maven:maven-plugin-api:3.8.1")
+    implementation("org.apache.maven.plugin-tools:maven-plugin-annotations:3.6.1")
+    implementation("org.apache.maven.plugins:maven-surefire-plugin:2.22.2")
+    implementation("org.twdata.maven:mojo-executor:2.3.2")
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+
+    implementation(project(":drill-cicd-common"))
+    implementation(project(":drill-cicd-github"))
+    implementation(project(":drill-cicd-gitlab"))
+}
+
 @Suppress("UNUSED_VARIABLE")
 tasks {
-    val clean by registering(Exec::class) {
-        val windows = HostManager.host.presetName == "mingwX64"
-        val args = if (windows) arrayOf("cmd", "/c", "mvnw.cmd") else arrayOf("sh", "./mvnw")
-        group = "build"
-        standardOutput = System.out
-        workingDir(projectDir)
-        commandLine(*args, "clean", "-Ddrill.plugin.version=$version", "-Dkotlin.version=$kotlinVersion")
+    val sourcesJar by registering(Jar::class) {
+        from(sourceSets.main.get().allSource)
+        from(project(":drill-cicd-common").sourceSets.main.get().allSource)
+        from(project(":drill-cicd-github").sourceSets.main.get().allSource)
+        from(project(":drill-cicd-gitlab").sourceSets.main.get().allSource)
+        archiveClassifier.set("sources")
     }
-    val install by registering(Exec::class) {
-        val windows = HostManager.host.presetName == "mingwX64"
-        val args = if (windows) arrayOf("cmd", "/c", "mvnw.cmd") else arrayOf("sh", "./mvnw")
-        group = "build"
-        standardOutput = System.out
-        workingDir(projectDir)
+    val javadocJar by registering(Jar::class) {
+        dependsOn(project(":drill-cicd-common").tasks.javadoc)
+        dependsOn(project(":drill-cicd-github").tasks.javadoc)
+        dependsOn(project(":drill-cicd-gitlab").tasks.javadoc)
+
+        from(javadoc.get().destinationDir)
+        from(project(":drill-cicd-common").tasks.javadoc.get().destinationDir)
+        from(project(":drill-cicd-github").tasks.javadoc.get().destinationDir)
+        from(project(":drill-cicd-gitlab").tasks.javadoc.get().destinationDir)
+        archiveClassifier.set("javadoc")
+    }
+    val mvnInstall by registering(Exec::class) {
+        val args = if (HostManager.hostIsMingw) arrayOf("cmd", "/c", "mvnw.cmd") else arrayOf("sh", "./mvnw")
         commandLine(*args, "install", "-Ddrill.plugin.version=$version", "-Dkotlin.version=$kotlinVersion")
+        workingDir(project.projectDir)
+        standardOutput = System.out
+        outputs.file("target/agent-runner-plugin-maven-$version.jar")
     }
-    publish.get().dependsOn(install)
-    publishToMavenLocal.get().dependsOn(install)
+    assemble.get().dependsOn(sourcesJar)
+    assemble.get().dependsOn(javadocJar)
+    assemble.get().dependsOn(mvnInstall)
+    clean {
+        delete("target")
+    }
 }
 
 publishing {
@@ -51,11 +86,19 @@ publishing {
             name.set("Maven plugin for CI/CD integration")
             description.set("Maven plugin for CI/CD integration")
             withXml {
-                asNode().appendNode("dependencies").appendNode("dependency").apply {
-                    appendNode("groupId", "org.jetbrains.kotlin")
-                    appendNode("artifactId", "kotlin-stdlib")
-                    appendNode("version", kotlinVersion)
-                }
+                asNode().appendNode("dependencies")
+                    .appendNode("dependency")
+                    .apply {
+                        appendNode("groupId", "org.jetbrains.kotlin")
+                        appendNode("artifactId", "kotlin-stdlib")
+                        appendNode("version", kotlinVersion)
+                    }
+                    .appendNode("dependency")
+                    .apply {
+                        appendNode("groupId", "org.jetbrains.kotlin")
+                        appendNode("artifactId", "kotlin-stdlib")
+                        appendNode("version", kotlinVersion)
+                    }
             }
         }
     }
