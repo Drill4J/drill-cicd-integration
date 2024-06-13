6528ced7cb13bfde7395699f0f1d46015b834f04
iryabov <to.iryabov@gmail.com>
2024-06-13 17:07:18 +0200

    feat: add a description to comparison report
    EPMDJ-10851

--- drill-cicd-common/src/main/kotlin/com/epam/drill/integration/common/report/impl/FormatUtils.kt
+++ drill-cicd-common/src/main/kotlin/com/epam/drill/integration/common/report/impl/FormatUtils.kt
@@ -20,3 +20,5 @@ fun Double.percent() = "%.0f".format(this * 100)
 fun String.wrapToLink(url: String?) = if (url != null) "[$this]($url)" else this
 
 fun Int.pluralEnding(ending: String = "s") = if (this > 1) ending else ""
+
+fun String.shortSha() = take(7)
