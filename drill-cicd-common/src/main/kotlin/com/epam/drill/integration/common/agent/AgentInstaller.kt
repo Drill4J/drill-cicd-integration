63a78e3ca44b56df29c7c63810f82d60786ec5ea
iryabov <to.iryabov@gmail.com>
2024-06-26 10:15:10 +0200

    feat: add `AgentInstaller` interface and `AgentInstallerImpl` implementation feat: add `AgentRunner` implementation test: add a unit test for `AgentInstallerImpl`
    EPMDJ-10862

---
+++ drill-cicd-common/src/main/kotlin/com/epam/drill/integration/common/agent/AgentInstaller.kt
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2020 - 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.epam.drill.integration.common.agent
+
+import java.io.File
+
+interface AgentInstaller {
+    suspend fun getDownloadUrl(githubRepository: String, versionMatching: String, osPreset: String): FileUrl?
+    suspend fun download(downloadUrl: FileUrl, downloadDir: Directory): File
+    fun unzip(zipFile: File): Directory
+    fun findAgentFile(unzippedDir: Directory, fileExtension: String): File?
+}
+
+class FileUrl(val url: String, val filename: String)
+typealias Directory = File
