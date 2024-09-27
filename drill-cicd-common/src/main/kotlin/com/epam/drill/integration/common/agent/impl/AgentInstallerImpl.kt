2d4cb062975114a7b919fe40300f44a9afc9d77e
iryabov <to.iryabov@gmail.com>
2024-09-27 11:44:27 +0200

    feat: add using GH_USER_TOKEN env to increase GitHub download limits
    EPMDJ-10927

--- drill-cicd-common/src/main/kotlin/com/epam/drill/integration/common/agent/impl/AgentInstallerImpl.kt
+++ drill-cicd-common/src/main/kotlin/com/epam/drill/integration/common/agent/impl/AgentInstallerImpl.kt
@@ -23,6 +23,7 @@ import io.ktor.client.features.json.*
 import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.utils.io.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonArray
@@ -33,9 +34,11 @@ import java.util.zip.ZipFile
 import kotlin.io.use
 
 private const val GITHUB_API_URL = "https://api.github.com"
+private const val GITHUB_USER_TOKEN = "GH_USER_TOKEN"
 
 class AgentInstallerImpl : AgentInstaller {
     private val json = Json { ignoreUnknownKeys = true }
+    private val token: String? = System.getenv(GITHUB_USER_TOKEN)
 
     var httpClient: HttpClient = HttpClient(CIO) {
         install(JsonFeature)
@@ -49,6 +52,9 @@ class AgentInstallerImpl : AgentInstaller {
 
         return httpClient.get<HttpResponse>(releasesUrl) {
             parameter("prerelease", true)
+            headers {
+                token?.let { append(HttpHeaders.Authorization, "Bearer $it") }
+            }
         }.let { response ->
             json.parseToJsonElement(response.readText())
         }.jsonArray.firstOrNull { release ->
@@ -77,7 +83,11 @@ class AgentInstallerImpl : AgentInstaller {
         }
         val file = File(downloadDir, downloadUrl.filename)
         if (!file.exists()) {
-            val response: HttpResponse = httpClient.get(downloadUrl.url)
+            val response: HttpResponse = httpClient.get(downloadUrl.url) {
+                headers {
+                    token?.let { append(HttpHeaders.Authorization, "Bearer $it") }
+                }
+            }
             val channel: ByteReadChannel = response.receive()
 
             file.outputStream().use { fileStream ->
